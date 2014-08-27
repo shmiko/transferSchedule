2d05f78c09328b89f739b55d781755b4a4106af1
MaximKovalchyk <ko.maxima@gmail.com>
2014-08-27 20:36:38 +0300

    add function getPageJSON

--- script/nulpIframeCut.js
+++ script/nulpIframeCut.js
@@ -1,10 +1,18 @@
 'use strict';
-(function (nulpIframe) {
-	nulpIframe.onload = function () {
-		console.log('nulpIframe.onload');
-		var nulpDocument = nulpIframe.contentWindow.document;
-		var mainPartOfPage = nulpDocument.getElementById('main');
-		nulpDocument.body.innerHTML = mainPartOfPage.outerHTML;
-		console.log('cut end!');
-	};
-} (document.getElementById('nulpIframe')));
+function runYQL(command, callback){
+	var callback_name = '__YQL_callback_'+(new Date()).getTime();
+	window[callback_name] = callback;
+	var a = document.createElement('script');
+	a.src = 'http://query.yahooapis.com/v1/public/yql?q=' + 
+	window.escape(command)+'&format=json&callback='+callback_name;
+	a.type = 'text/javascript';
+	document.getElementsByTagName('head')[0].appendChild(a);
+}
+
+getPageJSON('http://lp.edu.ua/node/40?inst=8&group=6563&semestr=0&semest_part=1',
+       function(data){console.log(data);}
+);
+
+function getPageJSON(url, callback) {
+	return runYQL('select * from html where url=\''+url+'\'', callback);
+}
