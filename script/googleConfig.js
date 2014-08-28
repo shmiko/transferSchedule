8df8d6300522167ef711795e54b612ee502d5d2f
MaximKovalchyk <ko.maxima@gmail.com>
2014-08-28 18:02:56 +0300

    Update googleConfig.js

--- script/googleConfig.js
+++ script/googleConfig.js
@@ -1,5 +1,5 @@
 window.scheduleTransfer.google.config = {
 	//563749310317-migfjpnl3bnetfjp19g1jqmkd2lmpsb8.apps.googleusercontent.com - for github
-	'client_id'		: '563749310317-eae8t8acnb2hrmnmrss0n2f8dff6g5un.apps.googleusercontent.com',
+	'client_id'		: '563749310317-migfjpnl3bnetfjp19g1jqmkd2lmpsb8.apps.googleusercontent.com',
 	'scope'			: 'https://www.googleapis.com/auth/calendar'
 };
